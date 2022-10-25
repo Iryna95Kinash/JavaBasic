ca8061f05d369a9829cb9c439d1d884f216af42a
IrynaKinash <irynakinash@gmail.com>
2022-10-25 18:25:41 +0300

    strings home work

--- src/primitives/AllPrimitiveTypes.java
+++ src/primitives/AllPrimitiveTypes.java
@@ -1,2 +1,27 @@
-package primitives;public class AllPrimitiveTypes {
+package primitives;
+
+public class AllPrimitiveTypes {
+    public static void main(String[] args) {
+
+        int addNumber = 75;
+        short addShort = 82;
+        long addLong = 84637;
+
+        double addDouble = 65.889;
+        float addFloat = 364.75f;
+
+        char addChar = 'x';
+        boolean addBoolean = true;
+
+        byte addByte = 122;
+
+        System.out.println(addNumber);
+        System.out.println(addShort);
+        System.out.println(addLong);
+        System.out.println(addDouble);
+        System.out.println(addFloat);
+        System.out.println(addChar);
+        System.out.println(addBoolean);
+        System.out.println(addByte);
+    }
 }
