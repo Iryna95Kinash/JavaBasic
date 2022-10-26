4895ff9dbdcf4c4803a221ee9dec2d76a871cdc2
IrynaKinash <irynakinash@gmail.com>
2022-10-26 14:11:20 +0300

    name and order fix

--- src/main/java/primitives/AllPrimitiveTypes.java
+++ src/main/java/primitives/AllPrimitiveTypes.java
@@ -3,25 +3,23 @@ package main.java.primitives;
 public class AllPrimitiveTypes {
     public static void main(String[] args) {
 
-        int addNumber = 75;
-        short addShort = 82;
-        long addLong = 84637;
+        boolean myBoolean = true;
+        byte myByte = 122;
+        short myShort = 82;
+        char myChar = 'x';
+        int myNum = 75;
+        float myFloat = 364.75f;
+        long myLong = 84637;
+        double myDoub = 65.889;
 
-        double addDouble = 65.889;
-        float addFloat = 364.75f;
 
-        char addChar = 'x';
-        boolean addBoolean = true;
-
-        byte addByte = 122;
-
-        System.out.println(addNumber);
-        System.out.println(addShort);
-        System.out.println(addLong);
-        System.out.println(addDouble);
-        System.out.println(addFloat);
-        System.out.println(addChar);
-        System.out.println(addBoolean);
-        System.out.println(addByte);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myChar);
+        System.out.println(myNum);
+        System.out.println(myFloat);
+        System.out.println(myLong);
+        System.out.println(myDoub);
     }
 }
