8b7516ca392e84930ea69c805c92ca7f2faf30bb
IrynaKinash <irynakinash@gmail.com>
2022-10-28 18:02:56 +0300

    added while and switch

--- src/main/java/userinput/userInput.java
+++ src/main/java/userinput/userInput.java
@@ -6,45 +6,41 @@ public class userInput {
     public static void main(String[] args) {
 
 
-
-
-        System.out.println("Hello user! \nIn this program you can press a number to fill your personal data: \nPress 1 to enter a name \nPress 2 to enter your age \nPress 3 to enter your height (in santimeters) \nPress 4 to enter your weight \nPress 5 to print your input \n\nEnter your number here:");
-
         String name = null;
         int age = 0;
         int height = 0;
         int weight = 0;
 
-        for (int i = 0; i < 5; i++) {
-            Scanner scanner = new Scanner(System.in);
-            int userInput = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int userInput = -1;
 
-            //while (userInput != 5) {
+        while (userInput != 5) {
 
-            if (userInput == 1) {
-                System.out.println("You fill " + userInput + "\nPlese, enter your name");
-                name = scanner.nextLine();
-                scanner.close();
-            } else if (userInput == 2) {
-                System.out.println("You fill " + userInput + "\nPlese, enter your age");
-                age = scanner.nextInt();
-            } else if (userInput == 3) {
-                System.out.println("You fill " + userInput + "\nPlese, enter your height (in santimeters)");
-                height = scanner.nextInt();
-            } else if (userInput == 4) {
-                System.out.println("You fill " + userInput + "\nPlese, enter your weight");
-                weight = scanner.nextInt();
-            } else if (userInput == 5) {
-                System.out.println("You fill " + userInput + "\nHere is your info:" + "Your name is " + name + ", you are " + age + " years old and you have" + height + " height and your weight is " + weight + " kg.\n");
-                System.out.println("Else");
-            } else {
-                System.out.println("Incorrect input, try again");
+            System.out.println("Hello user! \nIn this program you can press a number to fill your personal data: \nPress 1 to enter a name \nPress 2 to enter your age \nPress 3 to enter your height (in santimeters) \nPress 4 to enter your weight \nPress 5 to print your input \n\nEnter your number here:");
+            userInput = scanner.nextInt();
+
+            switch (userInput) {
+                case 1 -> {
+                    System.out.println("You fill " + userInput + "\nPlease, enter your name");
+                    name = scanner.next();
+                }
+                case 2 -> {
+                    System.out.println("You fill " + userInput + "\nPlease, enter your age");
+                    age = scanner.nextInt();
+                }
+                case 3 -> {
+                    System.out.println("You fill " + userInput + "\nPlease, enter your height (in santimeters)");
+                    height = scanner.nextInt();
+                }
+                case 4 -> {
+                    System.out.println("You fill " + userInput + "\nPlease, enter your weight");
+                    weight = scanner.nextInt();
+                }
+                default -> System.out.println("Incorrect input, try again");
             }
-//            int num = scanner.nextInt();
-//            System.out.println(num);
         }
 
-//        String userData = scanner.next();
-//        System.out.println("You fill " + userData);
+        System.out.println("You fill " + userInput + "\nHere is your info:" + "\nYour name is " + name + ", you are " + age + " years old and you have " + height + " height and your weight is " + weight + " kg.\n");
+        scanner.close();
     }
 }
