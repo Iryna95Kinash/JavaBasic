db5dd4b9bd428c456f93d3bacb33d390691d3294
IrynaKinash <irynakinash@gmail.com>
2022-11-01 17:03:35 +0200

    naming fix

--- src/main/java/arrays/Arrays.java
+++ src/main/java/arrays/Arrays.java
@@ -3,83 +3,83 @@ package main.java.arrays;
 public class Arrays {
     public static void main(String[] args) {
 
-        boolean[] myBoolean = new boolean[3];
-        byte[] myByte = new byte[4];
-        short[] myShort = new short[5];
-        char[] myChar = new char[6];
-        int[] myNum = new int[7];
-        float[] myFloat = new float[8];
-        long[] myLong = new long[2];
-        double[] myDoub = new double[1];
-        String[] myString = new String[10];
+        boolean[] arrBoolean = new boolean[3];
+        byte[] arrByte = new byte[4];
+        short[] arrShort = new short[5];
+        char[] arrChar = new char[6];
+        int[] arrNum = new int[7];
+        float[] arrFloat = new float[8];
+        long[] arrLong = new long[2];
+        double[] arrDoub = new double[1];
+        String[] arrString = new String[10];
 
-        myChar[0] = 'a';
-        myChar[1] = 'b';
-        myChar[2] = 'c';
-        myChar[3] = 'd';
-        myChar[4] = 'e';
-        myChar[5] = 'f';
+        arrChar[0] = 'a';
+        arrChar[1] = 'b';
+        arrChar[2] = 'c';
+        arrChar[3] = 'd';
+        arrChar[4] = 'e';
+        arrChar[5] = 'f';
 
-        myString[0] = "sunday";
-        myString[1] = "monday";
-        myString[2] = "tuesday";
-        myString[3] = "wednesday";
-        myString[4] = "thursday";
-        myString[5] = "friday";
-        myString[6] = "saturday";
-        myString[7] = "grapefruit";
-        myString[8] = "grape";
-        myString[9] = "kiwi";
+        arrString[0] = "sunday";
+        arrString[1] = "monday";
+        arrString[2] = "tuesday";
+        arrString[3] = "wednesday";
+        arrString[4] = "thursday";
+        arrString[5] = "friday";
+        arrString[6] = "saturday";
+        arrString[7] = "grapefruit";
+        arrString[8] = "grape";
+        arrString[9] = "kiwi";
 
 
-        for(int r = 0; r < myBoolean.length; r++) {
-            System.out.print(myBoolean[r] + " ");
+        for (boolean b : arrBoolean) {
+            System.out.print(b + " ");
         }
 
         System.out.println();
-        for (byte i = 0; i < myByte.length; i++) {
-            myByte[i] = (byte) (i + 100);
-            System.out.print(myByte[i] + " ");
+        for (byte i = 0; i < arrByte.length; i++) {
+            arrByte[i] = (byte) (i + 100);
+            System.out.print(arrByte[i] + " ");
         }
 
         System.out.println();
-        for (short j = 0; j < myShort.length; j++) {
-            myShort[j] = (short) (j + 1);
-            System.out.print(myShort[j] + " ");
+        for (short i = 0; i < arrShort.length; i++) {
+            arrShort[i] = (short) (i + 1);
+            System.out.print(arrShort[i] + " ");
         }
 
         System.out.println();
-        for (int i = 0; i<myChar.length; i++) {
-            System.out.print(myChar[i] + " ");
+        for (char c : arrChar) {
+            System.out.print(c + " ");
         }
 
         System.out.println();
-        for (int j = 0; j < myNum.length; j++) {
-            myNum[j] = j + 15;
-            System.out.print(myNum[j] + " ");
+        for (int i = 0; i < arrNum.length; i++) {
+            arrNum[i] = i + 15;
+            System.out.print(arrNum[i] + " ");
         }
 
         System.out.println();
-        for (float i = 0; i < myFloat.length; i++) {
-            myFloat[(int) i] = (float) (i + 0.579);
-            System.out.print(myFloat[(int) i] + " ");
+        for (float i = 0; i < arrFloat.length; i++) {
+            arrFloat[(int) i] = (float) (i + 0.579);
+            System.out.print(arrFloat[(int) i] + " ");
         }
 
         System.out.println();
-        for (long j = 0; j < myLong.length; j++) {
-            myLong[(int) j] = j + 2500;
-            System.out.print(myLong[(int) j] + " ");
+        for (long i = 0; i < arrLong.length; i++) {
+            arrLong[(int) i] = i + 2500;
+            System.out.print(arrLong[(int) i] + " ");
         }
 
         System.out.println();
-        for (double i = 0; i < myDoub.length; i++) {
-            myDoub[(int) i] = i + 1.33;
-            System.out.print(myDoub[(int) i] + " ");
+        for (double i = 0; i < arrDoub.length; i++) {
+            arrDoub[(int) i] = i + 1.33;
+            System.out.print(arrDoub[(int) i] + " ");
         }
 
         System.out.println();
-        for (int j = 0; j<myString.length; j++) {
-            System.out.print(myString[j] + " ");
+        for (String s : arrString) {
+            System.out.print(s + " ");
         }
     }
 }
