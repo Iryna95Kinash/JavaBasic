ec328dda080487e0329517e0c2ecc8315384d7c1
IrynaKinash <irynakinash@gmail.com>
2022-11-03 17:37:47 +0200

    hw6: method params, this, constructor

--- src/main/java/oop/App.java
+++ src/main/java/oop/App.java
@@ -4,8 +4,14 @@ public class App {
     public static void main(String[] args) {
 
         Car car = new Car();
+        Car car2 = new Car("Audi", "Germany");
+        Car car3 = new Car(200, 5, "Ukraine");
         Human h = new Human();
-        House house = new House();
+        Human h2 = new Human("Vika");
+        Human h3 = new Human(175, 55, 25)
+        House house = new House ();
+        House house2 = new House("red");
+        House house3 = new House("Halytskyi district", 1885, "brown");
 
         car.drive();
         car.startEngine();
@@ -13,6 +19,8 @@ public class App {
         car.drivingWithMaxSpeed();
         car.stopDriving();
         car.stopEngine();
+        car.includingPassengers(4);
+        car.madeCauntry("Sweden", "Volvo");
 
         System.out.println();
 
@@ -22,16 +30,21 @@ public class App {
         h.setAge(27);
         h.setAddress("Frankivskyi district");
 
+
         h.sayHello();
         h.introduce();
         h.sayGoodBye();
+        h.running(10);
+        h.birthIn("Ira", 1995);
 
         System.out.println();
 
         house.turnOnSerena();
-        house.setSerenaOff(true);
+        house.setSerenaOn(true);
         house.serenaOn();
-        house.setSerenaOff(false);
+        house.setSerenaOn(false);
         house.serenaOn();
+        house.locatedIn("Frankivskyi district");
+        house.builtIn("red", 1925);
     }
 }
